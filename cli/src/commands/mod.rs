ca3c980991b3560c7f052f9e839d2fa7aad27479
guitaripod <guitaripod@icloud.com>
2025-09-10 21:53:20 +0300

    feat: add video command modules to CLI
    - Add generate_video and edit_video modules to commands
    - Update module exports for video functionality

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1,13 +1,17 @@
 pub mod auth;
 pub mod config;
 pub mod edit;
+pub mod edit_video;
 pub mod generate;
+pub mod generate_video;
 pub mod health;
 pub mod info;
 
 pub use auth::{handle_login, handle_logout, handle_register, handle_status};
 pub use self::config::handle_config;
 pub use edit::handle_edit;
+pub use edit_video::handle_edit_video;
 pub use generate::handle_generate;
+pub use generate_video::handle_generate_video;
 pub use health::handle_health;
 pub use info::handle_info;
